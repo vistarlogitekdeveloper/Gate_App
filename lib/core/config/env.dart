9d1eda474287886891bb1bd88dbf4e60360039ac
vistarlogitekdeveloper <Flutter.developer@vistarlogitek.com>
2026-09-16 15:00:42 +0530

    feat: add Env configuration class for managing environment-based base URLs

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -6,16 +6,16 @@ class Env {
   static String get baseUrl {
     switch (current) {
       case Environment.dev:
-        // return 'https://api.vistarlogitek.com/api/v1/gate';
-        return 'https://uat-api.vistarlogitek.com/api/v1/gate';
+      return 'https://api.vistarlogitek.com/api/v1/gate';
+        //return 'https://uat-api.vistarlogitek.com/api/v1/gate';
 
       case Environment.stage:
-        // return 'https://api.vistarlogitek.com/api/v1/gate';
-        return 'https://uat-api.vistarlogitek.com/api/v1/gate';
+        return 'https://api.vistarlogitek.com/api/v1/gate';
+        //return 'https://uat-api.vistarlogitek.com/api/v1/gate';
 
       case Environment.prod:
-        // return 'https://api.vistarlogitek.com/api/v1/gate';
-        return 'https://uat-api.vistarlogitek.com/api/v1/gate';
+        return 'https://api.vistarlogitek.com/api/v1/gate';
+        //return 'https://uat-api.vistarlogitek.com/api/v1/gate';
     }
   }
 
